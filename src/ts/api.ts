467e348a7e3c7c3edb92e42c6e8fc28a4c14be78
Alisson <alisson.hdz.lopez@gmail.com>
2020-10-11 19:05:17 -0500

    Index Page

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -1,69 +1,27 @@
 /// <reference path="./models/country.ts"/>
 
-function main(): void {
-	renderCountries();
-}
+class Api {
+	constructor() {}
 
-document.addEventListener('DOMContentLoaded', (): void => {
-	main();
-});
-
-async function getAllCountries(): Promise<Country[] | null> {
-	try {
-		const res: Response = await fetch('https://restcountries.eu/rest/v2/all');
-		return (await res.json()) as Country[];
-	} catch (error) {
-		console.error('Error: ', error);
-		return null;
+	static async getAllCountries(): Promise<Country[] | null> {
+		try {
+			const res: Response = await fetch('https://restcountries.eu/rest/v2/all');
+			return (await res.json()) as Country[];
+		} catch (error) {
+			console.error('Error: ', error);
+			return null;
+		}
 	}
-}
 
-async function renderCountries(): Promise<void> {
-	const cardContainer: Element | null = document.querySelector('.card__container');
-	const countries = await getAllCountries();
-
-	console.log('works');
-
-	if (cardContainer && countries) {
-		countries.forEach((country) => {
-			cardContainer.appendChild(createCard(country));
-		});
-	} else if (!cardContainer) console.log('No Data');
-}
-
-function createCard(country: Country): Element {
-	const flag: HTMLImageElement = document.createElement('img');
-	flag.src = country.flag;
-	flag.alt = 'flag';
-
-	const cardImage: Element = document.createElement('div');
-	cardImage.classList.add('card__image');
-	cardImage.appendChild(flag);
-
-	const population: Element = document.createElement('p');
-	population.innerHTML = `<span class="text--semibold">Population: </span>${country.population}`;
-
-	const region: Element = document.createElement('p');
-	region.innerHTML = `<span class="text--semibold">Region: </span>${country.region}`;
-
-	const capital: Element = document.createElement('p');
-	capital.innerHTML = `<span class="text--semibold">Capital: </span>${country.capital}`;
-
-	const cardTitle: HTMLHeadElement = document.createElement('h2');
-	cardTitle.classList.add('card__title');
-	cardTitle.innerText = `${country.name}`;
-
-	const cardContent: Element = document.createElement('div');
-	cardContent.classList.add('card__content');
-	cardContent.append(population, region, capital);
-
-	const cardBody: Element = document.createElement('div');
-	cardBody.classList.add('card__body');
-	cardBody.append(cardTitle, cardContent);
-
-	const card: Element = document.createElement('div');
-	card.classList.add('card');
-	card.append(cardImage, cardBody);
-
-	return card;
+	static async getCountriesByRegion(region: string): Promise<Country[] | null> {
+		try {
+			const res: Response = await fetch(
+				`https://restcountries.eu/rest/v2/region/${region}`
+			);
+			return (await res.json()) as Country[];
+		} catch (error) {
+			console.error('Error: ', error);
+			return null;
+		}
+	}
 }
---
+++ src/ts/index.ts
@@ -0,0 +1,95 @@
+/// <reference path="./models/country.ts"/>
+/// <reference path="./api.ts"/>
+
+async function main(): Promise<void> {
+	const countrySelected: HTMLSelectElement | null = document.querySelector(
+		'.country__selected'
+	);
+	const searchInput: HTMLInputElement | null = document.querySelector('#search');
+
+	let countries: Country[] | null = await Api.getAllCountries();
+
+	renderCountries(countries);
+
+	countrySelected?.addEventListener(
+		'change',
+		async (): Promise<void> => {
+			const cardContainer: Element | null = document.querySelector('.card__container');
+			cardContainer?.remove();
+			countries = await Api.getCountriesByRegion(countrySelected.value);
+			renderCountries(countries);
+		}
+	);
+
+	searchInput?.addEventListener('input', searchCountry);
+}
+
+document.addEventListener('DOMContentLoaded', (): void => {
+	main();
+});
+
+async function renderCountries(countries: Country[] | null): Promise<void> {
+	const mainContent: Element | null = document.querySelector('#main-content');
+
+	const cardContainer: Element = document.createElement('section');
+	cardContainer.classList.add('card__container');
+	mainContent?.appendChild(cardContainer);
+
+	if (countries) {
+		countries.forEach((country) => {
+			cardContainer?.appendChild(createCard(country));
+		});
+	} else console.log('No Data');
+}
+
+// Searcher
+function searchCountry(this: HTMLInputElement): void {
+	const regExp: RegExp = new RegExp(this.value, 'i');
+	const countryCards: NodeListOf<Element> = document.querySelectorAll('.card');
+
+	countryCards.forEach((card: Element) => {
+		card.classList.add('hidden');
+		const countryName: string | null = card.childNodes[1].childNodes[0].textContent;
+
+		if (countryName?.replace(/\s/g, ' ').search(regExp) != -1) {
+			card.classList.remove('hidden');
+		}
+	});
+}
+
+function createCard(country: Country): Element {
+	const flag: HTMLImageElement = document.createElement('img');
+	flag.src = country.flag;
+	flag.alt = 'flag';
+
+	const cardImage: Element = document.createElement('div');
+	cardImage.classList.add('card__image');
+	cardImage.appendChild(flag);
+
+	const population: Element = document.createElement('p');
+	population.innerHTML = `<span class="text--semibold">Population: </span>${country.population}`;
+
+	const region: Element = document.createElement('p');
+	region.innerHTML = `<span class="text--semibold">Region: </span>${country.region}`;
+
+	const capital: Element = document.createElement('p');
+	capital.innerHTML = `<span class="text--semibold">Capital: </span>${country.capital}`;
+
+	const cardTitle: HTMLHeadElement = document.createElement('h2');
+	cardTitle.classList.add('card__title');
+	cardTitle.innerText = `${country.name}`;
+
+	const cardContent: Element = document.createElement('div');
+	cardContent.classList.add('card__content');
+	cardContent.append(population, region, capital);
+
+	const cardBody: Element = document.createElement('div');
+	cardBody.classList.add('card__body');
+	cardBody.append(cardTitle, cardContent);
+
+	const card: Element = document.createElement('div');
+	card.classList.add('card');
+	card.append(cardImage, cardBody);
+
+	return card;
+}
